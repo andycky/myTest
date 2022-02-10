443e28765a616fed4a73ee3dcf1ddf9d05cd95f2
andy.cheng <andy.cheng@uatlivibank.com>
2022-02-10 16:17:35 +0800

    update

--- src/Q3BoardGame.java
+++ src/Q3BoardGame.java
@@ -1,19 +1,15 @@
 /* Program in Java for finding possible ways of arriving exactly at the finishing point as mentioned above*/
-class Q3BoardGame
+class BroadGame2
 
 {
     // Returns number of ways to reach score n
     static int getCombination2(int space)
     {
         int combination = 0;
-        if (space == 1) {
-            combination = 1;
-        }
-
         for (int moved = 1; moved < space; moved++) {
             combination += getCombination2(space - moved);
         }
-        if (space >= 2 && space <=6) {
+        if (space >= 1 && space <=6) {
             combination +=1;
         }
         return combination;
@@ -25,9 +21,8 @@ class Q3BoardGame
         for (int n=1; n<=20; n++) {
             System.out.println("Number of possible ways to exactly move " + n + " spaces=" + getCombination2(n));
         }
-        //int n = 610;
-        //System.out.println("Number of possible ways to exactly move " + n + " spaces=" + getCombination2(n));
+        int n = 610;
+        System.out.println("Number of possible ways to exactly move " + n + " spaces=" + getCombination2(n));
     }
 
 }
-
